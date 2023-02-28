9eeb7b81f4f853687ee46f5c0772f0dbc7efd009
Anil Saxena Dev-1 <anil.saxena@gmx.us>
2023-02-28 17:38:35 +0530

    add initial commit

---
+++ src/main/java/com/quichstart/first_myproject/App.java
@@ -0,0 +1,13 @@
+package com.quichstart.first_myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
